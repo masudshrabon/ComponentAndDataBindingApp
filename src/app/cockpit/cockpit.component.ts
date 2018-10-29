45483f3ce18a2d99323bb4f239e639e2f75af238
masudshrabon <shrabon_bit0413@yahoo.com>
2018-10-29 23:10:25 +0600

    custom event binding

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-cockpit',
@@ -8,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class CockpitComponent implements OnInit {
     newServerName = '';
     newServerContent = '';
+    /**
+     * Make this event (event property) LISTENABLE from outside.
+     * Pass event (+ event data by $event) from CHILD to PARENT Component.
+    */
+    @Output()
+    serverCreated = new EventEmitter<{serverName: string, serverContent: string }>();
+    @Output()
+    blueprintCreated = new EventEmitter<{serverName: string, serverContent: string }>();
 
     constructor() { }
 
@@ -15,18 +23,20 @@ export class CockpitComponent implements OnInit {
     }
 
     onAddServer() {
-        // this.serverElements.push({
-        //     type: 'server',
-        //     name: this.newServerName,
-        //     content: this.newServerContent
-        // });
+      /**
+       * Emit this serverCreated (custome) event, i.e., the event has been occured
+       * just like the click (built-in) event.
+       * When the event (serverCreated) occured (or emitted),
+       * onServerAdded($event) will be executed as well as got the event data by $event.
+       * Example: app.component.html
+       * <app-cockpit (serverCreated) = "onServerAdded($event)"
+       * (blueprintCreated) = "onBlueprintAdded($event)"
+       * ></app-cockpit>
+       */
+        this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
     }
 
     onAddBlueprint() {
-        // this.serverElements.push({
-        //     type: 'blueprint',
-        //     name: this.newServerName,
-        //     content: this.newServerContent
-        // });
+        this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
     }
 }
