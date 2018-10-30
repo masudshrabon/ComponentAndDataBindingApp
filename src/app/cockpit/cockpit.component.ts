a98bafc867ca81db6b9f5f1a70275b69b8385777
masudshrabon <shrabon_bit0413@yahoo.com>
2018-10-30 22:25:08 +0600

    localRef and ViewChild used to get the html element

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,42 +1,65 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 
 @Component({
-    selector: 'app-cockpit',
-    templateUrl: './cockpit.component.html',
-    styleUrls: ['./cockpit.component.css']
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-    newServerName = '';
-    newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+  /** Accesss LocalRef #serverContentInput via @ViewChild decorator.
+   * @ViewChild can be used to accesss a component, like - XyzComponent (initial level).
+  */
+  @ViewChild('serverContentInput')
+  serverContentInput: ElementRef;
+  /**
+   * Make this event (event property) LISTENABLE from outside.
+   * Pass event (+ event data by $event) from CHILD to PARENT Component.
+   */
+  @Output()
+  serverCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  @Output()
+  blueprintCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onAddServer(nameInput: HTMLInputElement) {
     /**
-     * Make this event (event property) LISTENABLE from outside.
-     * Pass event (+ event data by $event) from CHILD to PARENT Component.
-    */
-    @Output()
-    serverCreated = new EventEmitter<{serverName: string, serverContent: string }>();
-    @Output()
-    blueprintCreated = new EventEmitter<{serverName: string, serverContent: string }>();
+     * Emit this serverCreated (custome) event, i.e., the event has been occured
+     * just like the click (built-in) event.
+     * When the event (serverCreated) occured (or emitted),
+     * onServerAdded($event) will be executed as well as got the event data by $event.
+     * Example: app.component.html
+     * <app-cockpit (serverCreated) = "onServerAdded($event)"
+     * (blueprintCreated) = "onBlueprintAdded($event)"
+     * ></app-cockpit>
+     */
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+  }
 
-    constructor() { }
-
-    ngOnInit() {
-    }
-
-    onAddServer() {
-      /**
-       * Emit this serverCreated (custome) event, i.e., the event has been occured
-       * just like the click (built-in) event.
-       * When the event (serverCreated) occured (or emitted),
-       * onServerAdded($event) will be executed as well as got the event data by $event.
-       * Example: app.component.html
-       * <app-cockpit (serverCreated) = "onServerAdded($event)"
-       * (blueprintCreated) = "onBlueprintAdded($event)"
-       * ></app-cockpit>
-       */
-        this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
-    }
-
-    onAddBlueprint() {
-        this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
-    }
+  onAddBlueprint(nameInput) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+  }
 }
